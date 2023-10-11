2f29d1927d75e1c446079e8355d753cf48e72242
Martin Rohrmeier <martin.ra.rohrmeier@bmw.de>
2023-10-11 09:38:11 +0200

    chore(version): set version and code beauty

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,3 +1,4 @@
+import { type PokemonListItem, useFetchPokemonsQuery } from 'features/api/pokemon'
 import { type Post, useFetchPostsQuery } from 'features/api/posts'
 import { type User, useFetchUsersQuery } from 'features/api/users'
 import ItemList from 'shared/ItemList'
@@ -5,26 +6,25 @@ import UserItem from './User'
 import PostItem from './Post'
 import PokemonItem from './Pokemon'
 import Notify from './Notify'
-import { PokemonListItem, useFetchPokemonsQuery } from 'features/api/pokemon'
 
-const renderUser = (item: User) => <UserItem item={item} />
-const renderPost = (item: Post) => <PostItem item={item} />
 const renderPokemon = (item: PokemonListItem) => <PokemonItem item={item} />
+const renderPost = (item: Post) => <PostItem item={item} />
+const renderUser = (item: User) => <UserItem item={item} />
 
 const App = () => {
   return (
     <main style={{ padding: '40px 100px' }}>
 
-      <h3>Users</h3>
-      <ItemList<User>
-        fetchItems={useFetchUsersQuery}
-        renderItem={renderUser}
-      />
-
       <h3>Pokemon</h3>
       <ItemList<PokemonListItem>
         fetchItems={useFetchPokemonsQuery}
         renderItem={renderPokemon}
+      />
+
+      <h3>Users</h3>
+      <ItemList<User>
+        fetchItems={useFetchUsersQuery}
+        renderItem={renderUser}
       />
 
       <h3>Posts</h3>
